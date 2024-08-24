a84e722e40f1f25445985df9a10630d1d0ffa4b3
libzdraz <zdraza@centrum.cz>
2024-08-24 20:05:46 +0200

    jen pokus
    učím se

---
+++ Clovek.java
@@ -0,0 +1,11 @@
+public class Clovek {
+ private String osoba;
+ 
+ public String getOsoba() {
+ return osoba;
+ }
+
+ public void setOsoba(String newOsoba) {
+ this.osoba = newOsoba;
+ }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      Clovek myObj = new Clovek();
+      myObj.setOsoba("John");
+      System.out.println(myObj.getOsoba());
+    }
+  }
